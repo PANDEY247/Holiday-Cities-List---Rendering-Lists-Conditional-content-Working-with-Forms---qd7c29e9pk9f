4c4597a1b4426fdc5b7aacf8cf3ccd7684a73707
sourabh pandey <57836486+PANDEY247@users.noreply.github.com>
2023-07-02 06:05:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,16 +24,46 @@ class App extends Component {
       { name: "Rome", country: "Italy" },
       { name: "Milan", country: "Italy" },
       { name: "Venice", country: "Italy" },
-      { name: "Varanasai", country: "India" },
+      { name: "Varanasi", country: "India" },
       { name: "Jaipur", country: "India" },
       { name: "The Hofburg", country: "Austria" },
       { name: "Belvedere Palace", country: "Austria" },
       { name: "St. Stephen Cathedral", country: "Austria" },
-      { name: "Kahna National Park", country: "India" },
+      { name: "Kanha National Park", country: "India" },
       { name: "Amritsar", country: "India" },
       { name: "Mussoorie", country: "India" },
       { name: "Mount Abu", country: "India" },
       { name: "Tirupati", country: "India" }
+    ];
+
+    this.validCities = [
+      "Goa",
+      "Amsterdam",
+      "New York",
+      "Darjeeling",
+      "Tokyo",
+      "Lonavala",
+      "Brandenburg Gate",
+      "Reichstag Building",
+      "Museum Island",
+      "Munnar",
+      "Leh Ladakh",
+      "Agra",
+      "Dalhousie",
+      "Coorg",
+      "Rome",
+      "Milan",
+      "Venice",
+      "Varanasi",
+      "Jaipur",
+      "The Hofburg",
+      "Belvedere Palace",
+      "St. Stephen Cathedral",
+      "Kanha National Park",
+      "Amritsar",
+      "Mussoorie",
+      "Mount Abu",
+      "Tirupati"
     ];
   }
 
@@ -41,11 +71,16 @@ class App extends Component {
     return (
       <div id="main">
         <ol>
-          {this.cityList.map((city, index) => (
-            <li key={`location${index + 1}`}>
-              {city.name}, {city.country}
-            </li>
-          ))}
+          {this.cityList.map((city, index) => {
+            if (this.validCities.includes(city.name)) {
+              return (
+                <li key={`location${index + 1}`}>
+                  {city.name}, {city.country}
+                </li>
+              );
+            }
+            return null; // Skip invalid cities
+          })}
         </ol>
       </div>
     );
